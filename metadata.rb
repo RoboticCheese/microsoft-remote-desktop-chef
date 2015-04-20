9f6f47c4362b436007f74008a151b44bf5ad8f5a
Jonathan Hartman <j@p4nt5.com>
2015-04-20 13:20:21 -0700

    Temp pin to dev version of mac-app-store

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ description      'Installs/Configures the Microsoft Remote Desktop app'
 long_description 'Installs/Configures the Microsoft Remote Desktop app'
 version          '0.0.1'
 
-depends          'mac-app-store', '~> 0.1'
+depends          'mac-app-store', '~> 0.0.1'
 
 supports         'mac_os_x'
 # rubocop:enable SingleSpaceBeforeFirstArg
